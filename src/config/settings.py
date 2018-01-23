e70bdaf35ef354f74c57f3eb3a9f80056857f76c
zooozoo <shz0309@gmail.com>
2018-01-23 12:20:14 +0900

    시작 전 커밋

--- src/config/settings.py
+++ src/config/settings.py
@@ -14,8 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
+ROOT_DIR = os.path.dirname(BASE_DIR)
+# Try-django-tutorial/src/templates/
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -26,7 +27,6 @@ SECRET_KEY = 'fr==n--th4&z0&#(3qfhsw2h8_w_$!h3hgmy5h%jh3@7^+5-u6'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -71,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -81,7 +80,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -101,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -114,7 +111,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,3 +1,33 @@
+import random
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(request):
+    num = None
+    some_list = [
+        random.randint(0, 100000),
+        random.randint(0, 100000)
+    ]
+    condition_bool_item = True
+    if condition_bool_item:
+        num = random.randint(0, 100000)
+    context = {
+        "num": num,
+        "some_list": some_list,
+    }
+    return render(request, "home.html", context)
+
+
+def about(request):
+    context = {
+    }
+    return render(request, "about.html", context)
+
+
+def contact(request):
+    context = {
+    }
+    return render(request, "contact.html", context)
