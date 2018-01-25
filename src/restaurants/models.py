f739f7ea36f5290228070e0178646188b0bfb38a
zooozoo <shz0309@gmail.com>
2018-01-25 18:29:31 +0900

    26 commit

--- src/restaurants/models.py
+++ src/restaurants/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 
 # Create your models here.
+from django.db.models.signals import pre_save
+
+from restaurants.utils import unique_slug_generator
+
+
 class RestaurantLocation(models.Model):
     name = models.CharField(max_length=120)
     location = models.CharField(max_length=120, blank=True, null=True)
@@ -16,3 +21,9 @@ class RestaurantLocation(models.Model):
     @property
     def title(self):
         return self.name
+
+def rl_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+pre_save.connect(rl_pre_save_receiver, sender=RestaurantLocation)
--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,14 +1,35 @@
 import random
 
 from django.db.models import Q
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.views import View
 from django.views.generic import TemplateView, ListView, DetailView
 
+from restaurants.forms import RestaurantCreateForm
 from restaurants.models import RestaurantLocation
+
+
+def restaurant_createview(request):
+    form =RestaurantCreateForm(request.POST or None)
+    errors = None
+    if form.is_valid():
+        obj = RestaurantLocation.objects.create(
+            name=form.cleaned_data.get('name'),
+            location=form.cleaned_data.get('location'),
+            category=form.cleaned_data.get('category')
+        )
+        return HttpResponseRedirect("/restaurants/")
+    if form.errors:
+        errors = form.errors
+    template_name = 'restaurants/form.html'
+    context = {
+        "form": form,
+        "errors": errors
+    }
+    return render(request, template_name, context)
 
 
 def restaurant_listview(request):
@@ -36,7 +57,7 @@ class RestaurantListView(ListView):
 class RestaurantDetailView(DetailView):
     queryset = RestaurantLocation.objects.all()
 
-    def get_object(self, *args, **kwargs):
-        rest_id = self.kwargs.get('rest_id')
-        obj = get_object_or_404(RestaurantLocation, id=rest_id)
-        return obj
+    # def get_object(self, *args, **kwargs):
+    #     rest_id = self.kwargs.get('rest_id')
+    #     obj = get_object_or_404(RestaurantLocation, id=rest_id)
+    #     return obj
