7ed603c6185eaf79e827f46f7a5eea6bab4b7084
zooozoo <shz0309@gmail.com>
2018-01-26 16:21:41 +0900

    menu items app

--- src/config/settings.py
+++ src/config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django_extensions',
 
     'restaurants',
+    'menus',
 ]
 
 MIDDLEWARE = [
---
+++ src/menus/models.py
@@ -0,0 +1,26 @@
+from django.conf import settings
+from django.db import models
+
+# Create your models here.
+from restaurants.models import RestaurantLocation
+
+
+class Item(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    restaurant = models.ForeignKey(RestaurantLocation)
+
+    name = models.CharField(max_length=120)
+    contents = models.TextField(help_text='separate each item by comma')
+    excludes = models.TextField(blank=True, null=True, help_text='separate each item by comma')
+    public = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-updated', '-timestamp']
+
+    def get_contents(self):
+        return self.contents.split(",")
+
+    def get_excludes(self):
+        return self.excludes.split(",")
