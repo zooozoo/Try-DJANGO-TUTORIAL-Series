e7c8c3fce310e28cecca2bc819ce790d1965747c
zooozoo <shz0309@gmail.com>
2018-01-30 12:00:14 +0900

    Limiting Form Field to QuerySet

--- src/menus/forms.py
+++ src/menus/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 from menus.models import Item
+from restaurants.models import RestaurantLocation
 
 
 class ItemForm(forms.ModelForm):
@@ -13,3 +14,9 @@ class ItemForm(forms.ModelForm):
             'excludes',
             'public'
         ]
+
+    def __init__(self, user=None, *args, **kwargs):
+        # print(kwargs.pop('user'))
+        print(user)
+        super(ItemForm, self).__init__(*args, **kwargs)
+        self.fields['restaurant'].queryset = RestaurantLocation.objects.filter(owner=user, item__isnull=True)
